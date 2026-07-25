7b8af24b527a017b4a6a1c187b3d883b937c1883
DiegoR <diegoerecarte@gmail.com>
2026-07-25 09:24:35 -0600

    new table

---
+++ src/lab1_recursividad_e4_programacion2/mainapp.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1_recursividad_e4_programacion2;
+
+/**
+ *
+ * @author user
+ */
+
+public class mainapp{
+    
+}
